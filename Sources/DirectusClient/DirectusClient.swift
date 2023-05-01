903d7208a3fc2ce9d9ced47d3c38576f177a0ac8
Kellvem Barbosa <kellvem222@gmail.com>
2023-05-01 23:04:21 +0100

    Update DirectusClient.swift

--- Sources/DirectusClient/DirectusClient.swift
+++ Sources/DirectusClient/DirectusClient.swift
@@ -1,6 +1,57 @@
 import Foundation
 import Combine
 
+
+/*
+ 
+// MARK: example
+ 
+```
+  Extensoes
+ extension DirectusClient {
+     func getFaces(page: Int, perPage: Int = 10, selectedTag: Tag? = nil) -> AnyPublisher<DirectusResults<[Watchface]>, Error> {
+         let endPoint = "/items/watchfaces"
+         let request = URLRequest(url: getBaseURL(endPoint: endPoint, customParams: "?fields=*,tags.tags_id&filter[_and][0][status][_eq]=published&filter[_and][1][faceModel][_in]=\(AuthViewModel.shared.getCurrentWatchModelCompatibles)&limit=\(perPage)&page=\(page)\(selectedTag != nil ? "&filter[_and][2][_and][2][tags][tags_id][id][_in]=\(selectedTag!.id)" : "")"))
+
+         print("request: \(String(describing: request.url))")
+
+         return agent.run(request)
+             .map(\.value)
+             .eraseToAnyPublisher()
+     }
+
+     func getTags(page: Int, perPage: Int = 10) -> AnyPublisher<DirectusResults<[Tag]>, Error> {
+         let endPoint = "/items/tags"
+         let request = URLRequest(url: getBaseURL(endPoint: endPoint, customParams: "?fields=id,name&filter[_and][0][status][_eq]=published&limit=\(perPage)&page=\(page)&sort=sort"))
+
+         print("request: \(String(describing: request.url))")
+
+         return agent.run(request)
+             .map(\.value)
+             .eraseToAnyPublisher()
+     }
+ }
+
+  Exemplo de uso:
+ let directusClient = DirectusClient(baseURL: URL(string: AppConfig.urlDirectus)!)
+
+ directusClient.getFaces(page: 1)
+     .sink(receiveCompletion: { completion in
+         switch completion {
+         case .failure(let error):
+             print("Error: \(error)")
+         case .finished:
+             print("Finished")
+         }
+     }, receiveValue: { result in
+         print("Result: \(result)")
+     })
+     .store(in: &cancellables)
+ ```
+ 
+ 
+ */
+
 public class DirectusClient {
     private let agent = Agent()
     let baseURL: URL
